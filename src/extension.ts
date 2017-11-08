093875fc603070e62682e724c7e3df96c4849f92
ole <ole.k@web.de>
2017-11-08 16:37:55 +0100

    make arma tools avaialble for other extensions using exports

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,16 @@
 import * as vscode from 'vscode';
 import { ArmaDev } from './armadev';
 import { ArmaDevCommands } from './commands';
+import * as armaTools from './helpers/armaTools';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
     new ArmaDev();
     new ArmaDevCommands(context);
+    // make available the armaToos for other extensions
+    // Usage: vscode.extensions.getExtension('ole1986.arma-dev').exports
+    return armaTools;
 }
 // this method is called when your extension is deactivated
 export function deactivate() {
